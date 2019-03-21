f2a8eaa32797f9890fa4cc1741050fff17e2a684
said9999 <jiangyaoxuan@gmail.com>
2019-03-21 18:46:14 +0800

    add read

--- one_byte.c
+++ one_byte.c
@@ -7,7 +7,8 @@
 #include <linux/types.h>
 #include <linux/fs.h>
 #include <linux/proc_fs.h>
-//#include <asm/uaccess.h>
+#include <linux/sched.h>
+#include <linux/uaccess.h>
 #define MAJOR_NUMBER 61
 
 static char *who = "default";
@@ -41,12 +42,21 @@ int onebyte_release(struct inode *inode, struct file *filep)
 count, loff_t *f_pos)
 {
      /*please complete the function on your own*/
-     return 0;
+     if (onebyte_data == NULL) {
+          return 0;
+     }
+     copy_to_user(buf, onebyte_data, 1);
+
+     onebyte_data = NULL;
+
+     return 1;
 }
 ssize_t onebyte_write(struct file *filep, const char *buf,
 size_t count, loff_t *f_pos)
 {
      /*please complete the function on your own*/
+     printk(KERN_ALERT "%s", buf);
+     printk(KERN_ALERT "%ld", count);
      return 0;
 }
 static int onebyte_init(void)
